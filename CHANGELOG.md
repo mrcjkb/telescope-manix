66feda7a276366edfc555cb0a7e56b8dd70aadc3
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-23 13:24:56 +0100

    chore: do not set a default telescope layout

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+- Do not set a default layout for telescope Hoogle search.
+
 ## [0.3.0] - 2022-10-19
 ### Added
 - Add option to search for word under cursor
--- lua/telescope-manix/init.lua
+++ lua/telescope-manix/init.lua
@@ -1,109 +1,101 @@
-local has_telescope, pickers = pcall(require, 'telescope.pickers')
-local finders = require('telescope.finders')
-local config = require('telescope.config').values
-local actions = require('telescope.actions')
-local actions_state = require('telescope.actions.state')
-local previewers = require('telescope.previewers')
-local previewer_utils = require('telescope.previewers.utils')
-local has_plenary, Job = pcall(require, 'plenary.job')
+local has_telescope, pickers = pcall(require, "telescope.pickers")
+local finders = require("telescope.finders")
+local config = require("telescope.config").values
+local actions = require("telescope.actions")
+local actions_state = require("telescope.actions.state")
+local previewers = require("telescope.previewers")
+local previewer_utils = require("telescope.previewers.utils")
+local has_plenary, Job = pcall(require, "plenary.job")
 
 if not has_telescope then
-  error('telescope-manix requires nvim-telescope/telescope.nvim')
+	error("telescope-manix requires nvim-telescope/telescope.nvim")
 end
 if not has_plenary then
-  error('telescope-manix requires nvim-lua/plenary.nvim')
+	error("telescope-manix requires nvim-lua/plenary.nvim")
 end
 
 local M = {}
 
 local function mk_manix_cmd(opts)
-  local search_arg = opts.cword and vim.fn.expand "<cword>" or ""
-  local args = opts.manix_args or {}
-  return vim.tbl_flatten { 'manix', search_arg, args, }
-end
-
-local function merge(...)
-  return vim.tbl_extend('keep', ...)
+	local search_arg = opts.cword and vim.fn.expand("<cword>") or ""
+	local args = opts.manix_args or {}
+	return vim.tbl_flatten({ "manix", search_arg, args })
 end
 
 local function tmatch(str, pattern)
-  local matches = {}
-  for match in string.gmatch(str, pattern) do
-    matches[#matches+1] = match
-  end
-  return matches
+	local matches = {}
+	for match in string.gmatch(str, pattern) do
+		matches[#matches + 1] = match
+	end
+	return matches
 end
 
 local function show_manix_preview(entry, buf)
-  Job:new({
-    command = 'manix',
-    args = { entry.display, '--strict'},
-    on_exit = function(job, exit_code)
-      vim.schedule(function()
-        if exit_code ~= 0 then
-          vim.notify('Error querying manix for manix-telescope preview', vim.log.levels.ERROR)
-          return
-        end
-        local manix_results = job:result()
-        if manix_results then
-          vim.api.nvim_buf_set_lines(buf, 0, -1, true, manix_results)
-          previewer_utils.highlighter(buf, 'nix')
-          vim.api.nvim_buf_call(buf, function()
-            local win = vim.fn.win_findbuf(buf)[1]
-            vim.wo[win].conceallevel = 2
-            vim.wo[win].wrap = true
-            vim.wo[win].linebreak = true
-            vim.bo[buf].textwidth = 80
-          end)
-        end
-      end)
-    end
-  }):start()
+	Job:new({
+		command = "manix",
+		args = { entry.display, "--strict" },
+		on_exit = function(job, exit_code)
+			vim.schedule(function()
+				if exit_code ~= 0 then
+					vim.notify("Error querying manix for manix-telescope preview", vim.log.levels.ERROR)
+					return
+				end
+				local manix_results = job:result()
+				if manix_results then
+					vim.api.nvim_buf_set_lines(buf, 0, -1, true, manix_results)
+					previewer_utils.highlighter(buf, "nix")
+					vim.api.nvim_buf_call(buf, function()
+						local win = vim.fn.win_findbuf(buf)[1]
+						vim.wo[win].conceallevel = 2
+						vim.wo[win].wrap = true
+						vim.wo[win].linebreak = true
+						vim.bo[buf].textwidth = 80
+					end)
+				end
+			end)
+		end,
+	}):start()
 end
 
 local function mk_manix_entry(entry)
-  local matches = tmatch(entry, '# ([%S]+)')
-  if #matches == 0 then
-    return nil
-  end
-  local display = matches[1]
-  return {
-    value = entry,
-    display = display,
-    ordinal = display,
-    preview_command = show_manix_preview
-  }
+	local matches = tmatch(entry, "# ([%S]+)")
+	if #matches == 0 then
+		return nil
+	end
+	local display = matches[1]
+	return {
+		value = entry,
+		display = display,
+		ordinal = display,
+		preview_command = show_manix_preview,
+	}
 end
 
 local function attach_mappings(buf)
-  actions.select_default:replace(function()
-    local entry = actions_state.get_selected_entry()
-    actions.close(buf)
-    vim.api.nvim_put({ entry.display }, "", true, true)
-  end)
-  return true
+	actions.select_default:replace(function()
+		local entry = actions_state.get_selected_entry()
+		actions.close(buf)
+		vim.api.nvim_put({ entry.display }, "", true, true)
+	end)
+	return true
 end
 
 M.search = function(opts)
-  if vim.fn.executable('manix') == '1' then
-    error("telescope-manix: 'manix' executable not found! Aborting.")
-    return
-  end
-  opts = merge(opts or {}, {
-    layout_strategy = 'horizontal',
-    layout_config = { preview_width = 80 }
-  })
-  opts.entry_maker = opts.entry_maker or mk_manix_entry
-  pickers.new(opts, {
-    prompt_title = 'Nix search',
-    finder = finders.new_oneshot_job(
-      mk_manix_cmd(opts),
-      opts
-    ),
-    sorter = config.generic_sorter(opts),
-    previewer = previewers.display_content.new(opts),
-    attach_mappings = attach_mappings,
-  }):find()
+	if vim.fn.executable("manix") == "1" then
+		error("telescope-manix: 'manix' executable not found! Aborting.")
+		return
+	end
+	opts = opts or {}
+	opts.entry_maker = opts.entry_maker or mk_manix_entry
+	pickers
+		.new(opts, {
+			prompt_title = "Nix search",
+			finder = finders.new_oneshot_job(mk_manix_cmd(opts), opts),
+			sorter = config.generic_sorter(opts),
+			previewer = previewers.display_content.new(opts),
+			attach_mappings = attach_mappings,
+		})
+		:find()
 end
 
 return M
